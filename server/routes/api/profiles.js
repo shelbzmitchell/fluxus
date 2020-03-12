2261c4c182da72c3685bbbea664a437d84070d8f
Shelby Mitchell <shelbzmitchell@gmail.com>
2020-03-12 13:21:07 -0700

    backend gets profiles and profile/id

---
+++ server/routes/api/profiles.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const profilesFile = __dirname + "/../../models/profiles.json";
+const profiles = require(profilesFile);
+// const helper = require("../../helper/helper");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const profileList = profiles.map(profile => {
+    return {
+      id: profile.id,
+      firstname: profile.firstname,
+      lastname: profile.lastname,
+      profilephoto: profile.profilephoto,
+      artphoto: profile.artphoto,
+      website: profile.website,
+      bio: profile.bio,
+      city: profile.city,
+      province: profile.province,
+      country: profile.country,
+      lookingtocollaborate: profile.lookingtocollaborate,
+      medium: profile.medium,
+      projects: profile.projects
+    };
+  });
+  res.json(profileList);
+});
+
+router.get("/:id", (req, res) => {
+  const found = profiles.some(profile => profile.id === req.params.id);
+  if (found) {
+    res.json(profiles.filter(profile => profile.id === req.params.id));
+  } else {
+    res.status(400).json({
+      errorMessage: `Cannot find Profile with ID:${req.params.id} not found`
+    });
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+const profileRoutes = require("./routes/api/profiles");
+app.use("/api/profiles", profileRoutes);
 
 app.listen(5000, () => {
   console.log("server running on port 5000");
